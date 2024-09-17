7d5b78ddd388b4e95d81db6fe2320945aab68721
Stefan <stefan.claas@mail.ru>
2024-09-18 00:01:38 +0200

    Add files via upload

--- mp.go
+++ mp.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
-func padText(text []byte) []byte {
+var paddingSize int
+
+func padText(text []byte, paddingSize int) []byte {
 	length := len(text)
-	paddingNeeded := 5 - (length % 5)
-	if paddingNeeded == 5 {
+	paddingNeeded := paddingSize - (length % paddingSize)
+	if paddingNeeded == paddingSize {
 		return text
 	}
 	
@@ -22,12 +25,23 @@ func padText(text []byte) []byte {
 }
 
 func printUsage() {
-	fmt.Fprintln(os.Stderr, "Usage: mp < infile > outfile")
-	fmt.Fprintln(os.Stderr, "\nThis program pads UTF-8 text to the nearest multiple of 5 bytes using 'X'.")
+	fmt.Fprintln(os.Stderr, "Usage: mp [-p 8] < infile > outfile")
+	fmt.Fprintln(os.Stderr, "\nThis program pads UTF-8 text to the nearest multiple of 5 (default) or 8 bytes using 'X'.")
 	fmt.Fprintln(os.Stderr, "It reads from stdin and writes to stdout, allowing use in pipelines.")
+	fmt.Fprintln(os.Stderr, "\nFlags:")
+	fmt.Fprintln(os.Stderr, "  -p 8\tPad to multiples of 8 instead of 5")
 }
 
 func main() {
+	flag.IntVar(&paddingSize, "p", 5, "Padding size (5 or 8)")
+	flag.Parse()
+
+	if paddingSize != 5 && paddingSize != 8 {
+		fmt.Fprintln(os.Stderr, "Invalid padding size. Use 5 (default) or 8.")
+		printUsage()
+		os.Exit(1)
+	}
+
 	// Check for -h flag or no input
 	if len(os.Args) > 1 && os.Args[1] == "-h" {
 		printUsage()
@@ -48,7 +62,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	paddedContent := padText(input)
+	paddedContent := padText(input, paddingSize)
 
 	_, err = os.Stdout.Write(paddedContent)
 	if err != nil {
