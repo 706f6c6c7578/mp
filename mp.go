57a850de9ea3aa7b2c2e8622683e30d26e24f604
Stefan <stefan.claas@mail.ru>
2024-11-21 17:03:27 +0100

    Add files via upload

--- mp.go
+++ mp.go
@@ -4,10 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
+	"time"
 )
 
 var paddingSize int
+
+func getRandomLetter() byte {
+	letters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	return letters[rand.Intn(len(letters))]
+}
 
 func padText(text []byte, paddingSize int) []byte {
 	length := len(text)
@@ -19,20 +26,22 @@ func padText(text []byte, paddingSize int) []byte {
 	paddedText := make([]byte, length+paddingNeeded)
 	copy(paddedText, text)
 	for i := length; i < len(paddedText); i++ {
-		paddedText[i] = 'X' // Padding with uppercase 'X'
+		paddedText[i] = getRandomLetter() // Padding with random uppercase letters
 	}
 	return paddedText
 }
 
 func printUsage() {
 	fmt.Fprintln(os.Stderr, "Usage: mp [-p 4] < infile > outfile")
-	fmt.Fprintln(os.Stderr, "\nThis program pads UTF-8 text to the nearest multiple of 5 (default) or 4 bytes using 'X'.")
+	fmt.Fprintln(os.Stderr, "\nThis program pads UTF-8 text to the nearest multiple of 5 (default) or 4 bytes using random uppercase letters.")
 	fmt.Fprintln(os.Stderr, "It reads from stdin and writes to stdout, allowing use in pipelines.")
 	fmt.Fprintln(os.Stderr, "\nFlags:")
 	fmt.Fprintln(os.Stderr, "  -p 4\tPad to multiples of 4 instead of 5")
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano()) // Initialize random seed
+	
 	flag.IntVar(&paddingSize, "p", 5, "Padding size (4 or 5)")
 	flag.Parse()
 
